aea465ad252a8765b473a23007f4be19ebf09af9
KevinDavilaDotCMS <kevin.davila@dotcms.com>
2023-12-10 00:36:18 -0500

    Published!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 interface Item {
-  name: string,
-};
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  item$: Observable<Item[]>;
-  firestore: Firestore = inject(Firestore);
-
-  constructor() {
-    const itemCollection = collection(this.firestore, 'items');
-    this.item$ = collectionData(itemCollection) as Observable<Item[]>;
-  }
+  constructor() {}
 }
